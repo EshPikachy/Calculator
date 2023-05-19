1e689d8476df59d7593e5a793cc9c88bdad01e4d
Chekelov Emir <garytobiazz03@gmail.com>
2023-05-19 19:48:35 +0600

    VER2

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector(".calculates");
 const input1 = document.querySelector(".firstinput");
 const input2 = document.querySelector(".secondinput");
 const values = document.querySelector(".calculatevalues");
+const values__normal = document.querySelector('.calculatevalues__normal')
 
 
 btn.addEventListener("click", () => {
@@ -15,3 +16,11 @@ function off() {
     input1.value = ''
     input2.value = ''
 }
+
+const btn2 = document.querySelector('.normal')
+
+btn.addEventListener('click', ()=>{
+    values__normal.innerHTML = `
+    <h2>   ${input1.value + input2.value} </h2>
+    `
+})
